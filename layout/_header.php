e4c818da0e9f4f30bb94277c854484bd6377f438
fanuel26 <fanuelperse@gmail.com>
2020-12-11 14:13:44 +0000

    relook finish

--- layout/_header.php
+++ layout/_header.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Noire relooking | <?= $title ?></title>
+    <title>Noire looking | <?= $title ?></title>
 
     <link rel="stylesheet" href="../asset/boot/css/bootstrap.min.css">
     <link rel="stylesheet" href="../asset/css/style.css">
